0f30397b971af80573964c23237bd6bd94875c65
Ruben <ruben@vita.io>
2019-12-14 16:11:19 +0100

    change int processor

--- 2019/src/day2.clj
+++ 2019/src/day2.clj
@@ -8,24 +8,48 @@
        vec
        delay))
 
-(defn execute-code [int-codes pointer]
-  (let [operator (nth int-codes pointer)
-        pos1 (nth int-codes (+ pointer 1) nil)
-        pos2 (nth int-codes (+ pointer 2) nil)
-        pos3 (nth int-codes (+ pointer 3) nil)]
-    (condp = operator
-      1 (assoc int-codes pos3 (+ (nth int-codes pos1) (nth int-codes pos2)))
-      2 (assoc int-codes pos3 (* (nth int-codes pos1) (nth int-codes pos2)))
-      99 nil)))
+(defn next-instruction [int-codes pointer]
+  (nth int-codes (swap! pointer inc) nil))
+
+(defn read-next-arg [int-codes pointer]
+  (nth int-codes
+       (next-instruction int-codes pointer)))
+
+(defn write-result [int-codes pointer result]
+  (assoc int-codes
+         (next-instruction int-codes pointer)
+         result))
+
+(defmulti execute-code
+  (fn [int-codes pointer]
+    (next-instruction int-codes pointer)))
+
+(defmethod execute-code 1 [int-codes pointer]
+  (write-result int-codes
+                pointer
+                (+ (read-next-arg int-codes pointer)
+                   (read-next-arg int-codes pointer))))
+
+(defmethod execute-code 2 [int-codes pointer]
+  (write-result int-codes
+                pointer
+                (* (read-next-arg int-codes pointer)
+                   (read-next-arg int-codes pointer))))
+
+(defmethod execute-code 99 [_ _]
+  nil)
+
+(defmethod execute-code :default [int-codes pointer]
+  (prn "Unknown code: " (nth int-codes @pointer)))
 
 (defn int-code-runner
   ([int-codes]
-   (int-code-runner int-codes 0))
+   (int-code-runner int-codes (atom -1)))
   ([int-codes pointer]
-   (let [result (execute-code int-codes pointer)]
-     (if (nil? result)
+   (let [memory (execute-code int-codes pointer)]
+     (if (nil? memory)
        int-codes
-       (int-code-runner result (+ 4 pointer))))))
+       (int-code-runner memory pointer)))))
 
 (def answer1
   (let [input (-> @numbers
